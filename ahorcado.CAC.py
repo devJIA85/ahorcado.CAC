c6a05a3e6fd0a32bf7536b11627d0ccde02d4250
Juan Ignacio A <j.antolini@me.com>
2023-05-30 09:37:10 -0300

    v 0.2
    Organizar Funciones

--- ahorcado.CAC.py
+++ ahorcado.CAC.py
@@ -6,3 +6,20 @@ palabras = ['python', 'programacion', 'ahorcado', 'juego', 'variables', 'pseint'
 # Función para seleccionar una palabra aleatoria
 def seleccionar_palabra():
     return random.choice(palabras)
+
+# Función para mostrar la figura del ahorcado
+
+
+# Función para adivinar la palabra
+
+
+# Función principal
+
+
+
+# Iniciar el juego
+jugar_ahorcado()
+
+
+
+# Esta aplicación permite jugar al juego del "Ahorcado" en la consola. Se selecciona una palabra aleatoria de una lista predefinida y el jugador debe adivinarla ingresando letras. La figura del ahorcado se muestra progresivamente a medida que el jugador adivina letras incorrectas. El juego finaliza cuando el jugador adivina la palabra o se completa la figura del ahorcado. También se lleva un registro de la puntuación del jugador, contabilizando el número de adivinanzas correctas e incorrectas.

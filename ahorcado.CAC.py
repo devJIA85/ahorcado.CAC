1165bf7be7b7b2829204ef8cd3f1c964e2fa9dce
Juan Ignacio A <j.antolini@me.com>
2023-05-30 09:35:32 -0300

    Create ahorcado.CAC.py

---
+++ ahorcado.CAC.py
@@ -0,0 +1,8 @@
+import random
+
+# Lista de palabras predefinidas
+palabras = ['python', 'programacion', 'ahorcado', 'juego', 'variables', 'pseint', 'pseudoclases', 'tupla', 'cadenas', 'buleanos']
+
+# Función para seleccionar una palabra aleatoria
+def seleccionar_palabra():
+    return random.choice(palabras)

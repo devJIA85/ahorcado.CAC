b3fef118fac8413892554606bb238b37380fb235
Juan Ignacio A <j.antolini@me.com>
2023-06-02 10:29:48 -0300

    v 0.4
    reunion 1

--- main.py
+++ main.py
@@ -77,11 +77,76 @@ def mostrar_ahorcado(intentos):
     print(figura[intentos])
 
 # Función para adivinar la palabra
+def adivinar_palabra(palabra):
+    palabra_adivinada = ['_'] * len(palabra)
+    intentos = 0
+    letras_adivinadas = []
 
+    while True:
+        print("\nPalabra:", ' '.join(palabra_adivinada))
+        letra = input("Ingresa una letra: ").lower()
+        mostrar_ahorcado(intentos)
+        print("Letras utilizadas:", ', '.join(letras_adivinadas))
+
+        if letra in letras_adivinadas:
+            print("Ya ingresaste esa letra. Intenta con otra.")
+            continue
+
+        letras_adivinadas.append(letra)
+
+        if letra in palabra:
+            for i in range(len(palabra)):
+                if palabra[i] == letra:
+                    palabra_adivinada[i] = letra
+        else:
+            intentos += 1
+            mostrar_ahorcado(intentos)
+            print("Letras utilizadas:", ', '.join(letras_adivinadas))
+
+        if ''.join(palabra_adivinada) == palabra:
+            print("\n¡Felicidades! ¡Adivinaste la palabra!")
+            print("La palabra era:", palabra)
+            return True
+
+        if intentos == 6:
+            print("\n¡Que pena! Perdiste!.")
+            print("La palabra era:", palabra)
+            return False
 
 # Función principal
+def jugar_ahorcado():
+    print("Bienvenido al juego del Ahorcado!")
+    puntuacion = {
+        'adivinanzas_correctas': 0,
+        'adivinanzas_incorrectas': 0
+    }
+    print( """
+           +---+
+           |   |
+               |
+               |
+               |
+               |
+        =========
+        """)
 
+    while True:
+        palabra = seleccionar_palabra()
+        print("\nNueva partida. Adivina la palabra:")
+        if adivinar_palabra(palabra):
+            puntuacion['adivinanzas_correctas'] += 1
+        else:
+            puntuacion['adivinanzas_incorrectas'] += 1
 
+        print("\nPuntuación actual")
+        print("\nAdivinanzas correctas:", puntuacion['adivinanzas_correctas'])
+        print("Adivinanzas incorrectas:", puntuacion['adivinanzas_incorrectas'])
+
+        continuar = input("\n¿Deseas jugar otra partida? (s/n): ").lower()
+        if continuar != 's':
+            break
+
+    print("\nGracias por jugar al Ahorcado!")
 
 # Iniciar el juego
 jugar_ahorcado()
